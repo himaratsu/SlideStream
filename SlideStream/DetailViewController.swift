b0a15e503ad90e62ad448ce509aba9513a0e55f3
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2015-09-18 13:39:02 +0900

    Added: Zoom Slide Image

--- SlideStream/DetailViewController.swift
+++ SlideStream/DetailViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import SafariServices
+import MWPhotoBrowser
 
-class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,
+MWPhotoBrowserDelegate {
 
     @IBOutlet weak private var hatenaCommentButton: UIBarButtonItem!
     @IBOutlet weak private var pageProgressView: PageProgressView!
     @IBOutlet weak var serviceIconItem: UIBarButtonItem!
     private var cellHeight: CGFloat = 270
+    
+    private var slideImages = [MWPhoto]()
     
     enum TableViewSectionType: Int {
         case Title = 0
@@ -128,6 +132,14 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         return TableViewSectionType.heightForCell(indexPath, slideCellHeight: cellHeight)
     }
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        switch indexPath.section {
+        case TableViewSectionType.SlideContent.rawValue:
+            showSlideZoom(indexPath.row)
+        default: ()
+        }
+    }
     
     // MARK: - ScrollViewDelegate
     
@@ -189,6 +201,73 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
             self.navigationController?.pushViewController(safariVC, animated: true)
         } else {
             UIApplication.sharedApplication().openURL(NSURL(string: slide!.link)!)
+        }
+    }
+    
+    
+    private func showSlideZoom(index: Int) {
+        
+        guard let slide = slide else {
+            return
+        }
+        
+        slideImages = [MWPhoto]()
+        
+        for index in 0...slide.totalCount {
+            if let slideUrl = slide.slideUrl(index) {
+                slideImages.append(MWPhoto(URL: NSURL(string: slideUrl)))
+            }
+        }
+        
+        let browser = MWPhotoBrowser(delegate: self)
+        navigationController?.pushViewController(browser, animated: true)
+        browser.setCurrentPhotoIndex(UInt(index))
+        
+        
+
+//        
+//        // Create browser (must be done each time photo browser is
+//        // displayed. Photo browser objects cannot be re-used)
+//        MWPhotoBrowser *browser = [[MWPhotoBrowser alloc] initWithDelegate:self];
+//        
+//        // Set options
+//        browser.displayActionButton = YES; // Show action button to allow sharing, copying, etc (defaults to YES)
+//        browser.displayNavArrows = NO; // Whether to display left and right nav arrows on toolbar (defaults to NO)
+//        browser.displaySelectionButtons = NO; // Whether selection buttons are shown on each image (defaults to NO)
+//        browser.zoomPhotosToFill = YES; // Images that almost fill the screen will be initially zoomed to fill (defaults to YES)
+//        browser.alwaysShowControls = NO; // Allows to control whether the bars and controls are always visible or whether they fade away to show the photo full (defaults to NO)
+//        browser.enableGrid = YES; // Whether to allow the viewing of all the photo thumbnails on a grid (defaults to YES)
+//        browser.startOnGrid = NO; // Whether to start on the grid of thumbnails instead of the first photo (defaults to NO)
+//        browser.autoPlayOnAppear = NO; // Auto-play first video
+//        
+//        // Customise selection images to change colours if required
+//        browser.customImageSelectedIconName = @"ImageSelected.png";
+//        browser.customImageSelectedSmallIconName = @"ImageSelectedSmall.png";
+//        
+//        // Optionally set the current visible photo before displaying
+//        [browser setCurrentPhotoIndex:1];
+//        
+//        // Present
+//        [self.navigationController pushViewController:browser animated:YES];
+//        
+//        // Manipulate
+//        [browser showNextPhotoAnimated:YES];
+//        [browser showPreviousPhotoAnimated:YES];
+//        [browser setCurrentPhotoIndex:10];
+    }
+    
+    // MARK: - MWPhotoBrowserDelegate
+    
+    func numberOfPhotosInPhotoBrowser(photoBrowser: MWPhotoBrowser!) -> UInt {
+        return UInt(slideImages.count)
+    }
+    
+    func photoBrowser(photoBrowser: MWPhotoBrowser!, photoAtIndex index: UInt) -> MWPhotoProtocol! {
+        let convertIndex = Int(index)
+        if convertIndex < slideImages.count {
+            return slideImages[convertIndex]
+        } else {
+            return nil
         }
     }
 }
